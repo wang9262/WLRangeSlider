40a67c642c00428fa9cca1256f23a40009d3d1c8
wang <wangle@meitu.com>
2014-09-01 23:26:07 +0800

    change demo.gif and the fold

--- CustomSlider/ViewController.m
+++ CustomSlider/ViewController.m
@@ -15,16 +15,6 @@
     _rangeSlider = [[WLRangeSlider alloc] initWithFrame:CGRectMake(20, 100, 280, 31)];
     [_rangeSlider addTarget:self action:@selector(valueChanged:) forControlEvents:UIControlEventValueChanged];
     [self.view addSubview:_rangeSlider];
-//    dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, 20 * NSEC_PER_SEC);
-//    dispatch_after(time, dispatch_get_main_queue()) {
-//        self.rangeSlider.trackHighlightTintColor = UIColor.redColor()
-//        self.rangeSlider.curvaceousness = 0.0
-//    };
-//    dispatch_after(time, dispatch_get_main_queue(), ^{
-//        rangeSlider.trackHighlightTintColor = [UIColor redColor];
-//        rangeSlider.thumbColor = [UIColor greenColor];
-//        rangeSlider.cornorRadiusScale = 0;
-//    });
     UISwitch *defaultSwitch = [[UISwitch alloc] init];
     [defaultSwitch addTarget:self action:@selector(switchCornorRadius:) forControlEvents:UIControlEventValueChanged];
     defaultSwitch.center = self.view.center;
--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,16 @@
+#Usage
+drag the `WLRangeSlider` fold to your project,and use the code as follows:
+
+	WLRangeSlider *rangeSlider = [[WLRangeSlider alloc] initWithFrame:CGRectMake(20, 100, 280, 31)];
+	[rangeSlider addTarget:self action:@selector(valueChanged:) forControlEvents:UIControlEventValueChanged];
+	[self.view addSubview: rangeSlider];
+	//you can change the left/right value,and set different color for track and left/right thumbs,also the cornorRadius of the thumb.like this
+	rangeSlider.trackHighlightTintColor = [UIColor redColor];
+	rangeSlider.thumbColor = [UIColor greenColor];
+	rangeSlider.cornorRadiusScale = 0;
+To find more,just try it out!Have fun,and give the pull reuest without hesitation!
+
+#TODO
+- use image to set the track and thumb.
+	
 ![](/demo.gif)
